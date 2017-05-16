1ea5878c022d29cbf8d52235a020831fb6aad813
Kundan Singh <kundan.singh979@gmail.com>
2017-05-16 23:46:58 +0530

    Created Routes

--- client/app.js
+++ client/app.js
@@ -1,4 +1,21 @@
 angular.module('InstaKilogram', ['ngRoute', 'ngMessages'])
     .config(function($routeProvider) {
-
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/home.html',
+                controller: 'HomeCtrl'
+            })
+            .when('/', {
+                templateUrl: 'views/login.html',
+                controller: 'LoginCtrl'
+            })
+            .when('/', {
+                templateUrl: 'views/signup.html',
+                controller: 'SignupCtrl'
+            })
+            .when('/photo/:id', {
+                templateUrl: 'views/detail.html',
+                controller: 'DetailCtrl'
+            })
+            .otherwise('/');
     });

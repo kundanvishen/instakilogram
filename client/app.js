4d7616400cbd7c631a468ce828743542645f34de
Kundan Singh <kundan.singh979@gmail.com>
2017-05-19 13:33:57 +0530

    OAuth integration

--- client/app.js
+++ client/app.js
@@ -1,5 +1,5 @@
-angular.module('InstaKilogram', ['ngRoute', 'ngMessages'])
-    .config(function($routeProvider) {
+angular.module('InstaKilogram', ['ngRoute', 'ngMessages', 'satellizer'])
+    .config(function($routeProvider, $authProvider) {
         $routeProvider
             .when('/', {
                 templateUrl: 'views/home.html',
@@ -9,13 +9,26 @@ angular.module('InstaKilogram', ['ngRoute', 'ngMessages'])
                 templateUrl: 'views/login.html',
                 controller: 'LoginCtrl'
             })
-            .when('/', {
-                templateUrl: 'views/signup.html',
-                controller: 'SignupCtrl'
-            })
-            .when('/photo/:id', {
-                templateUrl: 'views/detail.html',
-                controller: 'DetailCtrl'
-            })
+            // .when('/', {
+            //     templateUrl: 'views/signup.html',
+            //     controller: 'SignupCtrl'
+            // })
+            // .when('/photo/:id', {
+            //     templateUrl: 'views/detail.html',
+            //     controller: 'DetailCtrl'
+            // })
             .otherwise('/');
+
+        $authProvider.loginUrl = 'http://localhost:3000/auth/login';
+        $authProvider.signupUrl = 'http://localhost:3000/auth/signup';
+        $authProvider.oauth2({
+            name: 'instagram',
+            url: 'http://localhost:3000/auth/instagram',
+            redirectUri: 'http://localhost:8080',
+            clientId: '5b4480e4afe845aea09cb5899d2e6c2c',
+            requiredUrlParams: ['scope'],
+            scope: ['likes'],
+            scopeDelimiter: '+',
+            authorizationEndpoint: 'https://api.instagram.com/oauth/authorize'
+        });
     });

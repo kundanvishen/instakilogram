2089c215bb5a2ce97c84d65bd23b68761588e22f
Kundan Singh <kundan.singh979@gmail.com>
2017-12-10 00:06:50 +0530

    DB Setup

--- server/server.js
+++ server/server.js
@@ -1,5 +1,3 @@
-import { serveStatic } from '../../../../Library/Caches/typescript/2.6/node_modules/@types/serve-static';
-
 var bcrypt = require('bcryptjs');
 var bodyParser = require('body-parser');
 var cors = require('cors');
@@ -9,15 +7,36 @@ var moment = require('moment');
 var mongoose = require('mongoose');
 var path = require('path');
 var request = require('request');
+var config = require('./config');
 
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(serveStatic(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(app.get('port'), function(){
-	console.log('Server listening on port ' + app.get('port'));
+var User = mongoose.model('User', new mongoose.Schema({
+	instagramId: {type: String, index: true},
+	email: {type: String, unique: true, lowercase: true},
+	password: {type: String, select: false},
+	username: String,
+	fullName: String,
+	picture: String,
+	accessToken: String
+}));
+
+mongoose.connect(config.db);
+
+
+
+
+
+app.listen(app.get('port'), function () {
+	console.log('Express server listening on port ' + app.get('port'));
+});
+
+app.get('*', function(req, res){
+	res.send('Listening')
 })

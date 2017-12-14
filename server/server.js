5336d0a333d05bf7da78a5f81c14ba76971825ee
Kundan Singh <kundan.singh979@gmail.com>
2017-12-14 22:20:42 +0530

    Instagram API endpoints integration

--- server/server.js
+++ server/server.js
@@ -180,6 +180,36 @@ app.post('/auth/instagram', function(req, res){
 	})
 });
 
+app.post('/api/feed', function(req, res){
+	var feedUrl = 'https://api.instagram.com/v1/users/self/feed';
+	var params = {access_token: req.user.token};
+
+	request.get({
+		url: feedUrl, qs: params, json: true
+	}, function(error, response, body){
+		if(!error && response.statusCode == 200) {
+			res.send(body.data);
+		}
+	});
+});
+
+app.post('/api/like', isAuthenticated, function(req, res){
+	var mediaId = req.body.mediaId;
+	var access_token = {access_token: req.user.accessToken};
+	var likeUrl = 'https://api.instagram.com/v1/media/' + mediaId + '/likes';
+
+	req.post({url: likeUrl, form: accessToken, json: true}, function(error, response, body){
+		if(response.statusCode != 200){
+			return res.status(response.statusCode).send({
+				code: response.statusCode,
+				message: body.meta.error_message
+			});
+		}
+		res.status(200).end();
+	});
+});
+// 
+
 app.listen(app.get('port'), function () {
 	console.log('Express server listening on port ' + app.get('port'));
 });
